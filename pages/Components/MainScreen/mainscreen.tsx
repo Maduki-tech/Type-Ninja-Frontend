c44483ef373ad79acbbac61d04031e330a73d77a
Maduki-tech <maduki.business@gmail.com>
2022-03-08 10:09:20 +0100

    Main Struc of the Programm

---
+++ pages/Components/MainScreen/mainscreen.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const MainScreen: NextPage = () => {
+	return (
+		<div>
+			<h1>MainScreen</h1>
+		</div>
+	)
+}
+
+export default MainScreen;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,24 @@
 import type { NextPage } from "next";
+import MainScreen from "./Components/MainScreen/mainscreen";
+import Footer from "./Components/Footer/footer";
+import Head from "next/head";
+import Header from './Components/Header/header'
 
 const Home: NextPage = () => {
   return (
     <div>
-      <h1>Init</h1>
+      <div>
+        <Head>
+          <title>Type Ninja</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
+      </div>
+		<Header/>
+		<MainScreen/>
+		<Footer/>
     </div>
   );
 };
